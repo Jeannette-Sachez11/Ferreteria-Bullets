7c146c4fa6fee7723c33a9ed7e6769ea8bbc164b
Jeannette-Sachez11 <jeannette.sanchez79@unach.mx>
2022-05-16 05:41:08 -0500

    Final

--- src/Vista/Ingreso.java
+++ src/Vista/Ingreso.java
@@ -200,7 +200,7 @@ public class Ingreso extends javax.swing.JFrame {
                    
                    
                    Ventana_admin nf= new Ventana_admin();
-                   Ventana_caj vc= new Ventana_caj();
+                   
                     if(Nombre_Usu.equals("jeannette_admin")&&Contra_Usu.equals("adminJeannette"))
                     {
                        nf.setVisible(true);
@@ -208,14 +208,6 @@ public class Ingreso extends javax.swing.JFrame {
                     }else if(Nombre_Usu.equals("noe_admin")&&Contra_Usu.equals("adminNoe"))
                     {
                        nf.setVisible(true);
-                       this.dispose();
-                    }else if(Nombre_Usu.equals("anahi_cajera")&&Contra_Usu.equals("cajeraAna"))
-                    {
-                       vc.setVisible(true);
-                       this.dispose();
-                    }else if(Nombre_Usu.equals("natalia_cajera")&&Contra_Usu.equals("cajeraNati"))
-                    {
-                       vc.setVisible(true);
                        this.dispose();
                     }else{
                         JOptionPane.showMessageDialog(rootPane, "Usuario o contraseña incorrectos!!");
